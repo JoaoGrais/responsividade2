85cf79753c107307d439af8ef647cd528e462836
JoaoGrais <115734114+JoaoGrais@users.noreply.github.com>
2023-05-15 16:25:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuHamburger =  document.
+querySelector('.menu-hamburger');
+
+const menu = document.querySelector('.menu');
+
+menuHamburger.addEventListener('click', () => {
+    menu.classList.toggle('active')
+} );
